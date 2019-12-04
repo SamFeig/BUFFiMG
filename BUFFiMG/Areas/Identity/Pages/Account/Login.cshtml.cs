19867931fc84cc8c0063cee0876af08d95f6a46a
meigs2 <ConnorRadeloff1@gmail.com>
2019-12-04 15:37:22 -0700

    Update login to work properly using a username instead of an email.

--- BUFFiMG/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ BUFFiMG/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -46,8 +46,8 @@ namespace BUFFiMG.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+            [Display(Name = "User Name")]
+            public string UserName { get; set; }
 
             [Required]
             [DataType(DataType.Password)]
@@ -82,7 +82,7 @@ namespace BUFFiMG.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: true);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
@@ -115,10 +115,11 @@ namespace BUFFiMG.Areas.Identity.Pages.Account
                 return Page();
             }
 
-            var user = await _userManager.FindByEmailAsync(Input.Email);
+            var user = await _userManager.FindByNameAsync(Input.UserName);
             if (user == null)
             {
                 ModelState.AddModelError(string.Empty, "Verification email sent. Please check your email.");
+                return Page();
             }
 
             var userId = await _userManager.GetUserIdAsync(user);
@@ -129,7 +130,7 @@ namespace BUFFiMG.Areas.Identity.Pages.Account
                 values: new { userId = userId, code = code },
                 protocol: Request.Scheme);
             await _emailSender.SendEmailAsync(
-                Input.Email,
+                user.Email,
                 "Confirm your email",
                 $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
