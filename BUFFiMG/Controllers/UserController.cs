4700e06f0ba847dcf5a186ab15aa0232f7ea4eff
Sam Feig <spfeig@gmail.com>
2019-12-04 19:30:37 -0700

    User Image View
    Added controller and model for the user view of all their uploaded images

--- BUFFiMG/Controllers/UserController.cs
+++ BUFFiMG/Controllers/UserController.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Authorization;
+using BUFFiMG.Models;
+using System.IO;
+using System.Security.Claims;
 
 namespace BUFFiMG.Controllers
 {
@@ -17,11 +20,30 @@ namespace BUFFiMG.Controllers
             {
                 return View("Error");
             }
-            return View();
+            var user = User.FindFirst(ClaimTypes.NameIdentifier);
+            var userId = user.Value;
+            var db = new BUFFiMG.Data.buffimgContext();
+            var currentUser = db.AspNetUsers.SingleOrDefault(u => u.Id == userId);
+
+            if(currentUser == null){
+                return View("Error");
+            }
+
+            var photos = currentUser.Photos;
+
+            var ImageModel = new UserImagesModel();
+
+            foreach(var photo in photos) {
+                var path = Path.Combine("/user_images/", photo.FilePath + photo.FileExtension);
+
+                ImageModel.imageList.Add(new DisplayImage(){src=path, tags=new List<string>()});
+                }
+
+            return View(ImageModel);
         }
 
-        // 
-        // GET: /HelloWorld/Welcome/ 
+        //
+        // GET: /HelloWorld/Welcome/
 
         public IActionResult Welcome(string name, int numTimes = 1)
         {
